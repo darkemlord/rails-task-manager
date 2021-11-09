17f53b553cb247e924185609daa67fe83307f734
emanuel caro <darkemlord@gmail.com>
2021-11-09 17:00:49 +0900

    CRUD Done missing style

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # READ ALL
+  get 'tasks', to: 'tasks#index', as: :tasks
+  # CREATE NEW TASK GET FORM GET AND POST
+  get 'tasks/new', to: 'tasks#new', as: :new
+  post 'tasks', to: 'tasks#create'
+  # READ ONE
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # UPDATE
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update'
+  # DELETE
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
